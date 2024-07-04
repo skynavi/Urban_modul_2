99b6f01cafd5d02b710381fbffdd56760fe370a9
skynavi <skynavi@mail.ru>
2024-07-04 19:56:28 +0300

    end

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first != second != third and first != third != second:
+    print('Все числа разные "0"')
+elif first == second == third:
+    print('Все 3 числа равны')
+elif (first == second or
+      second == third or
+      first == third):
+    print('2 числа равны')
