df6183f9df1313c085ee3e969d0ef069979a8614
skynavi <skynavi@mail.ru>
2024-07-07 19:28:05 +0300

    я это сделал.

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    b = (my_list[a])
+    if b > 0:
+        print(b)
+    elif b < 0:
+        break
+    a = a + 1
+
+
+
+
