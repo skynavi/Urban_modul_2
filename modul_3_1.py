6ed6f21b60059e998bf68794b6af2602694d5c24
skynavi <skynavi@mail.ru>
2024-07-12 20:15:22 +0300

    исправления

--- modul_3_1.py
+++ modul_3_1.py
@@ -1,35 +1,36 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
-    return calls
+
 
 count_calls()
 
 
-
-def string_info():
-    string_1 = input('введите строку: ')
+def string_info(string_1):
     len_ = len(string_1)
-    tuple = (len_, string_1.upper(), string_1)
-    print(tuple)
+    tup_ = (len_, string_1.upper(), string_1.lower())
+    print(tup_)
     return string_1
-    count_calls() +1
 
 
-string_info()
+count_calls()
 
-while True: #  я не понял как сделать циклическое выполнение, поэтому так...
-    a = input('поиск: ')
-    b = ['ban', 'BaNaN', 'urBAN']
-    def is_contains(c, e):
-        if c in e:
+string_info('Capybara')
+
+
+def is_contains(string, list_to_search):
+    for i in list_to_search:
+        if string in i:
             print('True')
-
-        if c not in e:
+        else:
             print('Fals')
-        count_calls() + 1
+        return i
 
 
-    is_contains(a, b)
-    print(calls)
+count_calls()
+
+is_contains('ban', ['ban', 'BaNaN', 'urBAN'])
+print(calls)
