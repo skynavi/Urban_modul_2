7230630fd5b632a6491bfdcc3440576ca01211e5
skynavi <skynavi@mail.ru>
2024-07-13 15:55:52 +0300

    добавил перевод в нижний регистр, убрал else

--- modul_3_1.py
+++ modul_3_1.py
@@ -12,21 +12,24 @@ count_calls()
 def string_info(string_1):
     len_ = len(string_1)
     tup_ = (len_, string_1.upper(), string_1.lower())
+    count_calls()
     return tup_
 
 
-count_calls()
+
+#count_calls()
 
 
 def is_contains(string, list_to_search):
-    for i in list_to_search:
+    list_to_search_low = [s.lower() for s in list_to_search]
+    for i in list_to_search_low:
         if string in i:
             return True
-        else:
-            return False
+    count_calls()
 
 
-count_calls()
+
+#count_calls()
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
