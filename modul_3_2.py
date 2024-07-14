d6c484133e17d47b67458dab11f720e43e9f7566
skynavi <skynavi@mail.ru>
2024-07-14 16:32:07 +0300

    dc

---
+++ modul_3_2.py
@@ -0,0 +1,17 @@
+# рассылка писем (способы вызова функций)
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    if not (recipient.endswith((".com", ".ru", ".net")) and '@' in recipient and sender.endswith(
+            (".com", ".ru", ".net")) and '@' in sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
